b71d0f7967f12f80fe7eaee2d9761598711af2af
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-08-02 11:32:40 -0300

    Projeto criado

---
+++ src/test/java/com/leonardo/projetospring/ProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.projetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
